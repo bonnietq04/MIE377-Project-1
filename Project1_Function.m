316cec57f848e1873ffcff662c9a18772da5747b
bhavisaa <162748988+bhavisaa@users.noreply.github.com>
2025-03-13 12:13:07 -0400

    Add files via upload

--- Project1_Function.m
+++ Project1_Function.m
@@ -1,14 +1,25 @@
 function x = Project1_Function(periodReturns, periodFactRet, x0)
 
-    % Use this function to implement an optimized algorithmic trading strategy
-    % using a Mean-Variance Optimization (MVO) framework with factor models (OLS).
+    % Use this function to implement your algorithmic asset management
+    % strategy. You can modify this function, but you must keep the inputs
+    % and outputs consistent.
+    %
     % INPUTS: periodReturns, periodFactRet, x0 (current portfolio weights)
-    % OUTPUT: x (optimized portfolio weights)
-    
-    % Define estimation period (most recent 3 years for calibration)
-    returns = periodReturns(max(1, end-59):end, :);
-    factRet = periodFactRet(max(1, end-59):end, :);
-    
+    % OUTPUTS: x (optimal portfolio)
+    %
+    % An example of an MVO implementation with OLS regression is given
+    % below. Please be sure to include comments in your code.
+    %
+    % *************** WRITE YOUR CODE HERE ***************
+    %----------------------------------------------------------------------
+
+    % Example: subset the data to consistently use the most recent 5 years
+    % Define estimation period (most recent 5 years for calibration)
+    returns = periodReturns(max(1,end-59):end,:);
+    factRet = periodFactRet(max(1,end-59):end,:);
+    %returns (isnan(returns)) = 0;
+    %factRet(isnan(factRet) | isinf(factRet)) = 0;
+
     % UNCOMMENT TO RUN BSS
     %selectedFactors = BSS(returns, factRet)
     %[mu, Q] = OLS(returns, factRet(:, selectedFactors));
@@ -22,9 +33,6 @@ function x = Project1_Function(periodReturns, periodFactRet, x0)
     %selectedFactors = LASSO(returns, factRet)
     %[mu, Q] = OLS(returns, factRet(:, selectedFactors));
 
-    % UNCOMMENT TO RUN REGULAR OLS
-    %[mu, Q] = OLS(returns, factRet);
-
     
     % UNCOMMENT TO RUN CAPM OLS
     %[mu, Q] = OLS_CAPM(returns, factRet);
@@ -32,7 +40,16 @@ function x = Project1_Function(periodReturns, periodFactRet, x0)
     % UNCOMMENT TO RUN FAMA FRENCH OLS
     %[mu, Q] = OLS_FamaFrench(returns, factRet);
     
-    % Optimize portfolio using MVO Model B (Maximize Return with Variance Constraint)
-    x = MVO(mu, Q, x0); % for Max Return MVO Model 
+    % comment this out to run average / uncomment to run factors
+    [mu, Q] = OLS(returns, factRet);
+    %disp(mu);
+
+    % uncomment to run average
+    %mu = mean(periodReturns)';  % n x 1
+    %Q  = cov(periodReturns);    % n x n
     
+    % Example: Use MVO to optimize our portfolio
+    x = RobustMVO(mu, Q, x0);
+
+    %----------------------------------------------------------------------
 end
